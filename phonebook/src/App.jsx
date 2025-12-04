7789b491b31ba6fe24e6418ea3e425436dcd5d13
Mika7596 <mika7596@gmail.com>
2025-12-04 17:05:54 +0100

    Finished exercise 2.17

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -14,11 +14,13 @@ function App() {
   const [filterValue, setFilterValue] = useState("");
   const [showAddingSuccessMessage, setShowAddingSuccessMessage] = useState(false);
   const [showUpdateSuccessMessage, setShowUpdateSuccessMessage] = useState([false, null]);
+  const [showDeleteSuccessMessage, setShowDeleteSuccessMessage] = useState([false, null]);
+  const [showErrorMessage, setShowErrorMessage] = useState([false, null])
 
   useEffect(() =>{
     const getAllFunc = peopleService.getAllPeople();
     getAllFunc.then(resp => setPersons(resp.data));
-  }, [persons])
+  }, [])
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -37,9 +39,15 @@ function App() {
       const message = `${newName} is already added to phonebook. Replace the old number with the new one?`
       if(confirm(message)) {
         const updatedUser = {...repeatedName, number: newNumber}
-        peopleService.updatePerson(updatedUser.id, updatedUser);
-        setShowUpdateSuccessMessage([true, updatedUser]);
-        return setTimeout(()=>{setShowUpdateSuccessMessage([false, null])}, 5000)
+        peopleService.updatePerson(updatedUser.id, updatedUser)
+        .then(resp =>{
+          setShowUpdateSuccessMessage([true, updatedUser]);
+          return setTimeout(()=>{setShowUpdateSuccessMessage([false, null])}, 4000)
+        })
+        .catch(err => {
+          setShowErrorMessage([true, updatedUser.name]);
+          return setTimeout(()=>{setShowErrorMessage([false, null])}, 4000)
+        });
       }
     }
 
@@ -48,13 +56,23 @@ function App() {
     setPersons([...persons, newObject]);
     peopleService.createPerson(newObject);
     setShowAddingSuccessMessage(true);
-    setTimeout(()=>{setShowAddingSuccessMessage(false)}, 5000)
+    setTimeout(()=>{setShowAddingSuccessMessage(false)}, 4000)
   };
 
   const deletePerson = id => {
     const userToDelete = persons.find(person => person.id == id)
     if (confirm(`Are you sure you want to delete ${userToDelete.name}`)){
-      peopleService.deletePerson(id);
+      peopleService.deletePerson(id)
+      .then(resp => {
+        console.log(resp.data.name)
+        setShowDeleteSuccessMessage([true, resp.data.name])
+        setTimeout(()=>{setShowDeleteSuccessMessage([false, null])}, 4000)
+      })
+      .catch(err => {
+        console.log(err, userToDelete);
+        setShowErrorMessage([true, userToDelete.name]);
+        setTimeout(() => {setShowErrorMessage([false, null])}, 4000)
+      });
     }
   }
 
@@ -63,6 +81,8 @@ function App() {
       <h2>Phonebook</h2>
       {showAddingSuccessMessage && (<div className="success-msg">Added {persons[persons.length-1].name} successfully!</div>)}
       {showUpdateSuccessMessage[0] && (<div className="success-msg">Updated {showUpdateSuccessMessage[1].name}'s phone number successfully!</div>)}
+      {showDeleteSuccessMessage[0] && (<div className="success-msg">Deleted {showDeleteSuccessMessage[1]} from phonebook successfully!</div>)}
+      {showErrorMessage[0] && (<div className="error-msg">Cannot find {showErrorMessage[1]} on the phonebook, seems like it's been already deleted.</div>)}
       <Filter filterValue={filterValue} setFilterValue={setFilterValue}/>
       <h1>Add a new one</h1>
       <AddingForm handleSubmit={handleSubmit} newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber}></AddingForm>
